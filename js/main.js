345690eda5c15d9b028ecb5340b06915639d3c76
rohit malakar <rohitmalakar@rohits-MacBook-Pro.local>
2017-03-16 09:58:14 +0545

    background added in game.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 var game = new Phaser.Game(640, 360, Phaser.AUTO);
+
 var GameState = {
+	// load the game assets before the game starts
 	preload: function(){
-
+		this.load.image('background', 'assets/images/background.jpg');
 	},
+	// executed after everything is loaded
 	create: function(){
-
+		this.background = this.game.add.sprite(0, 0, 'background');
 	},
 	update: function() {
 		
